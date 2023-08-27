dfb0ce747d7146fffed0b44233591512d4bec58e
IncogNET <company@incognet.io>
2023-08-27 05:42:02 -0400

    Update result.php

--- result.php
+++ result.php
@@ -1,16 +1,14 @@
 <?php
-// Setting content type to plain text for better display of command outputs.
 header('Content-Type: text/plain; charset=utf-8');
-
-// Setting the execution time to 60 seconds as some commands may take longer.
+// Ensure this script can run for a while, some commands might take time.
 set_time_limit(60);
-
 /**
  * Function to check if an IP address is within a specified subnet.
  * @param string $ip - The IP address to check.
  * @param string $range - The subnet range.
  * @return bool - Returns true if IP is in the range, false otherwise.
  */
+// Check to see if an IP is within a specified subnet (Bogons)
 function ip_in_range($ip, $range) {
     // If range does not contain a CIDR, it's assumed to be a /32.
     if (strpos($range, '/') === false) {
@@ -18,7 +16,7 @@ function ip_in_range($ip, $range) {
     }
 
     list($range, $netmask) = explode('/', $range, 2);
-
+    
     // Checking for IPv6 addresses.
     if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
         // Convert IP and range to big integers for comparison.
@@ -28,7 +26,7 @@ function ip_in_range($ip, $range) {
         // Calculating wildcards and netmask for IPv6.
         $wildcard_dec = gmp_sub(gmp_pow(2, 128), 1);
         $netmask_dec = gmp_sub(gmp_pow(2, 128), gmp_pow(2, (128 - $netmask)));
-
+        
         $ip_net = gmp_and($ip_dec, $netmask_dec);
         $range_net = gmp_and($range_dec, $netmask_dec);
 
@@ -37,7 +35,7 @@ function ip_in_range($ip, $range) {
 
     // Checking for IPv4 addresses.
     if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-        // Convert IP and range to integer values for comparison.
+        // Convert IP and range to integer values for compariso
         $ip_dec = ip2long($ip);
         $range_dec = ip2long($range);
 
@@ -48,32 +46,70 @@ function ip_in_range($ip, $range) {
         return (($ip_dec & $netmask_dec) == ($range_dec & $netmask_dec));
     }
 
-    // Default case if IP does not match IPv4 or IPv6.
     return false;
 }
 
 // List of forbidden subnets to check against.
-$forbiddenSubnets = [ ... ];
+$forbiddenSubnets = [
+    '0.0.0.0/8',
+    '10.0.0.0/8',
+    '100.64.0.0/10',
+    '127.0.0.0/8',
+    '169.254.0.0/16',
+    '172.16.0.0/12',
+    '192.0.0.0/24',
+    '192.0.2.0/24',
+    '192.168.0.0/16',
+    '198.18.0.0/15',
+    '198.51.100.0/24',
+    '203.0.113.0/24',
+    '::1/128',
+    '::/128',
+    '::ffff:0:0/96',
+    '100::/64',
+    '2001::/23',
+    '2001:db8::/32',
+    'fe80::/10'
+];
 
-// Get IP and command from POST request.
-$ip = filter_input(INPUT_POST, 'ip', FILTER_VALIDATE_IP);
-$command = $_POST['command'] ?? null;
 
-// Basic validation for provided IP.
-if (!isset($_POST['ip']) || empty($_POST['ip'])) {
+// Get the raw IP or hostname from the POST request
+$input = $_POST['ip'] ?? null;
+
+// Basic validation for provided IP/hostname.
+if (!isset($input) || empty($input)) {
     die("IP/Hostname is required.");
 }
 
-// Check if provided IP is in any forbidden subnets.
-$forbidden = false;
-$matchedSubnet = '';  // for debugging purposes
-foreach ($forbiddenSubnets as $subnet) {
-    if (ip_in_range($ip, $subnet)) {
-        $forbidden = true;
-        $matchedSubnet = $subnet;  // store the matched subnet for debug.
-        break;
+// Check for other forbidden values that could exploit SSRF vulnerabilities.
+$forbiddenValues = ['localhost'];
+foreach ($forbiddenValues as $value) {
+    if (strpos($input, $value) !== false) {
+        die("Invalid IP/Hostname.");
     }
 }
+
+// If the input is a valid IP, check if it's in any forbidden subnets.
+if (filter_var($input, FILTER_VALIDATE_IP)) {
+    $forbidden = false;
+    $matchedSubnet = '';  // for debugging purposes
+    foreach ($forbiddenSubnets as $subnet) {
+        if (ip_in_range($input, $subnet)) {
+            $forbidden = true;
+            $matchedSubnet = $subnet;  // store the matched subnet for debug.
+            break;
+        }
+    }
+    if ($forbidden) {
+        die('The IP address or subnet you have entered is not allowed. Matched subnet: ' . $matchedSubnet);
+    }
+}
+
+// Then, continue with the rest of your logic to get the command and execute it.
+
+$command = $_POST['command'] ?? null;
+
+
 if ($forbidden) {
     die('The IP address or subnet you have entered is not allowed. Matched subnet: ' . $matchedSubnet);
 }
@@ -81,13 +117,20 @@ if ($forbidden) {
 // Check for other forbidden values that could exploit SSRF vulnerabilities.
 $forbiddenValues = ['localhost'];
 foreach ($forbiddenValues as $value) {
-    if (strpos($ip, $value) !== false) {
+    if (strpos($input, $value) !== false) {
         die("Invalid IP/Hostname.");
     }
 }
 
 // List of allowed commands and their shell equivalents.
-$allowedCommands = [ ... ];
+$allowedCommands = [
+    'ping' => 'ping -c 20',
+    'ping6' => 'ping6 -c 20',
+    'traceroute' => 'traceroute',
+    'traceroute6' => 'traceroute6',
+    'mtr' => 'mtr --report --report-cycles 10',
+    'mtr6' => 'mtr --report --report-cycles 10 -6'
+];
 
 // Check if provided command is in allowed list.
 if (!isset($allowedCommands[$command])) {
@@ -95,7 +138,7 @@ if (!isset($allowedCommands[$command])) {
 }
 
 // Construct the final shell command, escaping the IP for security.
-$finalCommand = $allowedCommands[$command] . ' ' . escapeshellarg($ip) . ' 2>&1';
+$finalCommand = $allowedCommands[$command] . ' ' . escapeshellarg($input) . ' 2>&1';
 
 // Execute the command and display the output.
 $output = shell_exec($finalCommand);
